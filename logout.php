edefd05be7f22d330c864215d98ab0edde682bfb
ChadS <32550318+ChadS@users.noreply.github.com>
2019-03-14 18:02:08 -0400

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION["username"]);
-   unset($_SESSION["password"]);
-   session_destroy();
-   
-   echo 'You have cleaned session';
-   header('Refresh: 2; URL = php_login.php');
-?>
